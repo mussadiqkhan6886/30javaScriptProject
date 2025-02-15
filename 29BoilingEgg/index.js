b9837f7c9b3295e23d348b9709c69f6a43627c00
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-02-15 17:20:46 +0500

    added boiling egg project and also started calories calculator

---
+++ 29BoilingEgg/index.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('button');
+const timerDiv = document.getElementById('timer');
+const result = document.getElementById('result');
+const animation = document.getElementById('animation');
+let intervalId;
+let m = 8;
+let s =  0;
+const boilEgg = () => {
+    clearInterval(intervalId);
+    result.innerHTML = `Egg boiling`;
+
+    animation.style.display = 'inline-block';
+    if(s == 0){
+        m -= 1;
+        s = 59;
+    }else{
+        s -= 1;
+    }
+    timerDiv.innerHTML = `${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+    intervalId = setInterval(boilEgg, 10);
+    if(m == 0 && s == 0){
+        clearInterval(intervalId);
+        result.innerHTML = "Egg Boiled⏰";
+        timerDiv.innerHTML = `00:00`;
+        animation.style.display = 'none';
+    }
+    
+};
+
+button.addEventListener('click', boilEgg);
+
